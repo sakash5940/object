65a9a1cc299c77642d544849321a8833a264f7cb
sakash5940 <akashpatil5940@gmail.com>
2024-09-28 19:02:13 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,15 @@
+//var salary=50000
+//var inc=10000
+//console.log(salary+inc)
+//console.log(salary-inc)
+//var mnth=12
+//console.log(salary*mnth)
+//var package=600000
+//var mnth=12
+//var day=22
+//console.log(package/mnth/day)
+//var attendedmnth=--mnth
+//var mnth=11
+//var attendedmnth=++mnth
+//console.log(attendedmnth)
+var
